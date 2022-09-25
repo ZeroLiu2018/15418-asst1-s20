d8345995498bb9cd61b27beb901f1c3db135aa8e
zeroliu <997556859@qq.com>
2022-09-26 00:42:22 +0800

    p5 done

--- prob5_cuberoot/data.cpp
+++ prob5_cuberoot/data.cpp
@@ -10,15 +10,19 @@ void initRandom(float *values, int N) {
 
 // Generate data that gives high relative speedup
 void initGood(float *values, int N) {
-  for (int i = 0; i < N; i++) {
+  float interval = 1.0 / N;
+  for (int i = 0; i < N; i += 8) {
+    auto v = 0.f + interval * i;
+    for (int j = 0; j < 8; ++j) {
+      values[i + 1] = v;
+    }
   }
 }
 
 // Generate data that gives low relative speedup
 void initBad(float *values, int N) {
-  for (int i = 0; i < N; i++) {
-    // TODO: Choose data values that will yield low speedup
-    values[i] = 1.f;
+  for (int i = 0; i < N; ++i) {
+    values[i] = i % 8 == 0 ? 0.f : 1.f;
   }
 }
 
